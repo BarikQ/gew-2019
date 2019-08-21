d1b8bf5a3d418f56761aa7f72df3cdf01a9025fd
BarikQ <yar.barik99@gmail.com>
2019-08-22 00:47:04 +0300

    speakers

--- src/scripts/speakers.js
+++ src/scripts/speakers.js
@@ -1,3 +1,23 @@
 $(document).ready(function () {
-  
+  let path='./src/images/speakers/';
+
+  $('.radio-label').click(function() {
+    const speakersLists = document.querySelectorAll('.speakers-list');
+    const activeLabel = this.classList[1];
+
+    speakersLists.forEach(elem => {
+      if (elem.classList.contains('active')) elem.classList.remove('active');
+
+      if (elem.classList.contains(activeLabel)) {
+        const speakers = elem.querySelectorAll('.speaker__image');
+        console.log(speakers);
+
+        speakers.forEach((elem, i) => {
+          elem.style.background = `url('${path}${activeLabel}/sp-${i+1}.png')`;
+          elem.style.backgroundSize = 'cover';
+        });
+        elem.classList.add('active');
+      }
+    });
+  });
 });
