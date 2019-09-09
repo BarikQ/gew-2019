376b4c3925e262c6cd89d790b86f71f6755d76dc
BarikQ <yar.barik99@gmail.com>
2019-09-09 18:57:06 +0300

    feat: slick arrows

--- src/scripts/expo.js
+++ src/scripts/expo.js
@@ -58,16 +58,14 @@ $(document).ready(function () {
         breakpoint: 600,
         settings: {
           slidesToShow: 2,
-          slidesToScroll: 2,
-          arrows: false
+          slidesToScroll: 2
         }
       },
       {
         breakpoint: 330,
         settings: {
           slidesToShow: 1,
-          slidesToScroll: 1,
-          arrows: false
+          slidesToScroll: 1
         }
       }
     ]
